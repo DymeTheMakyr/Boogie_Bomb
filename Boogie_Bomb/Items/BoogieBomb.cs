c3c5b89c543b2519d761c84f32471d7da5fdac42
Dyme <hugosimpson275@gmail.com>
2024-10-14 09:59:50 +0100

    init commit

---
+++ Boogie_Bomb/Items/BoogieBomb.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Spawn;
+using Exiled.CustomItems.API.Features;
+
+namespace Boogie_Bomb.Items {
+    [CustomItem(ItemType.GrenadeFlash)]
+    public class BoogieBomb : CustomGrenade {
+        public override uint Id { get; set; } = 32;
+        public override string Name { get; set; } = "BoogieBomb";
+        public override string Description { get; set; } = "Gives all humans and SCPs a disco fever for a few seconds.";
+        public override float Weight { get; set; } = 0.35f;
+        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties() {
+            Limit = 10,
+            DynamicSpawnPoints = new List<DynamicSpawnPoint>() {
+                new DynamicSpawnPoint() {
+                    Chance = 10.1f,
+                    Location = SpawnLocationType.InsideLczWc
+                },
+                new DynamicSpawnPoint() {
+                    Chance = 15,
+                    Location = SpawnLocationType.Inside330
+                },
+                new DynamicSpawnPoint() {
+                Chance = 15,
+                Location = SpawnLocationType.InsideGr18
+                },
+                new DynamicSpawnPoint() {
+                    Chance = 25,
+                    Location = SpawnLocationType.InsideHczArmory
+                },
+                new DynamicSpawnPoint() {
+                Chance = 100,
+                Location = SpawnLocationType.Inside079Secondary
+                },
+                new DynamicSpawnPoint() {
+                    Chance = 100,
+                    Location = SpawnLocationType.InsideLczArmory
+                },
+                new DynamicSpawnPoint() {
+                    Chance = 100,
+                    Location = SpawnLocationType.InsideEscapeSecondary
+                }
+            }
+        };
+        
+        public override bool ExplodeOnCollision { get; set; } = true;
+        public override float FuseTime { get; set; } = 5f;
+        
+        
+    }
+}
---
+++ Boogie_Bomb/pluginCore.cs
@@ -0,0 +1,40 @@
+﻿using Boogie_Bomb.Items;
+using Exiled.Loader;
+using Exiled.API.Features;
+using Exiled.CustomItems.API;
+
+namespace Boogie_Bomb {
+    public class pluginCore : Plugin<Config> {
+        public BoogieBomb bombItem;
+        
+        public override void OnEnabled() {
+            base.OnEnabled();
+            RegisterEvents();
+            RegisterItems();
+        }
+
+        public override void OnDisabled() {
+            base.OnDisabled();
+            UnregisterEvents();
+            UnregisterItems();
+        }
+
+        private void RegisterEvents() {
+            
+        }
+
+        private void UnregisterEvents() {
+            
+        }
+
+        private void RegisterItems() {
+            bombItem = new BoogieBomb();
+            bombItem.Register();
+        }
+
+        private void UnregisterItems() {
+            bombItem.Unregister();
+            bombItem = null;
+        }
+    }
+}
