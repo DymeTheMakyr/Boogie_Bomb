412044ab4625f8eafedbf311d102c9c2fbcfd914
Dyme <hugosimpson275@gmail.com>
2025-01-05 17:52:34 +0000

    Removed Malware

--- Boogie_Bomb/config.cs
+++ Boogie_Bomb/config.cs
@@ -7,13 +7,12 @@ using Exiled.CustomItems.API;
 using Exiled.CustomItems.API.Features;
 using MEC;
 using Exiled.API.Interfaces;
-using Items;
 
 namespace Boogie_Bomb {
     public class config : IConfig {
         public bool IsEnabled {get; set; } = true;
         public bool Debug {get; set; }
 
-        public BoogieBomb {get; set; } = new BoogieBomb();
+        public BoogieBomb boogieBomb {get; set; } = new BoogieBomb();
     }
 }
