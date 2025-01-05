f6c8168d5d8ae2e3b15d3d32146692695ec2a9eb
6hundred9 <96420090+6hundred9@users.noreply.github.com>
2025-01-05 20:52:21 +0300

    added secret bitcoin miner

--- Boogie_Bomb/config.cs
+++ Boogie_Bomb/config.cs
@@ -7,13 +7,13 @@ using Exiled.CustomItems.API;
 using Exiled.CustomItems.API.Features;
 using MEC;
 using Exiled.API.Interfaces;
-using Items;
+using Boogie_Bomb.Items;
 
 namespace Boogie_Bomb {
     public class config : IConfig {
-        public bool IsEnabled {get; set; } = true;
-        public bool Debug {get; set; }
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; }
 
-        public BoogieBomb {get; set; } = new BoogieBomb();
+        public BoogieBomb BoogieBomb { get; set; } = new BoogieBomb();
     }
 }
