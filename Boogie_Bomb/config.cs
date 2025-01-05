5cdb330cec1f1c535b39dcf21fe5168fc2415255
6hundred9 <96420090+6hundred9@users.noreply.github.com>
2025-01-05 20:38:05 +0300

    Update config.cs

--- Boogie_Bomb/config.cs
+++ Boogie_Bomb/config.cs
@@ -6,9 +6,14 @@ using Exiled.CustomItems;
 using Exiled.CustomItems.API;
 using Exiled.CustomItems.API.Features;
 using MEC;
+using Exiled.API.Interfaces;
+using Items;
 
 namespace Boogie_Bomb {
     public class config : IConfig {
-        
+        public bool IsEnabled {get; set; } = true;
+        public bool Debug {get; set; }
+
+        public BoogieBomb {get; set; } = new BoogieBomb();
     }
 }
